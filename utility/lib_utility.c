2c25da01a6bfd8ae8b68ea919e183da9b65df9dc
Daniele Albanese <daniele.albanese98@gmail.com>
2021-01-23 00:33:01 +0100

    Added funtion get_max and get_min

--- utility/lib_utility.c
+++ utility/lib_utility.c
@@ -43,3 +43,19 @@ int is_equal_str(char x[], char y[]) {
 
     return FALSE;
 }
+
+int get_max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int get_min(int x, int y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
--- utility/lib_utility.h
+++ utility/lib_utility.h
@@ -10,5 +10,7 @@
 int get_str_lenght(char x[]);
 char *get_sub_str(char *x, int start, int num_char);
 int is_equal_str(char x[], char y[]);
+int get_max(int x, int y);
+int get_min(int x, int y);
 
 #endif //MY_C_LIBRARY_LIB_UTILITY_H
